e9492c5cc9213076f68ac7bca5700b0263d7d6bd
project <139109816+seyyedparsalotfy@users.noreply.github.com>
2024-03-14 14:33:57 +0000

    update endddd

--- parsa/src/App.js
+++ parsa/src/App.js
@@ -4,7 +4,7 @@ const ExampleComponent = () => {
   const data = {
     trip_financials: [
       {
-        id: 2867462,
+        trip_id: 2867462,
         request_datetime: "2022-04-20T17:08:36.465861+04:30",
         driver: "راننده تست",
         final_price: 40000,
@@ -32,21 +32,39 @@ const ExampleComponent = () => {
         id: 199069,
         datetime: "2022-04-20T14:57:09.959629+04:30",
         amount: -100000,
-        description: null,
+        description:"payments" ,
       },
       {
-        id: 199070,
+       id: 199070,
         datetime: "2022-04-18T16:58:47.678934+04:30",
         amount: -7140000,
-        description: null,
+        description: "payments",
       },
     ],
+    
+    misc_expenses: [
+      {
+        misc_id: 10740,
+        description: "تست فنی",
+        datetime: "2022-04-20T16:57:30.703642+04:30",
+          amount: 120000
+      }
+  ],
+  concurrency_costs: [
+      {
+        concurrency_id: 28705,
+        datetime : "2022-04-20T16:58:54.148113+04:30",
+          amount: 7140000,
+          start_date: "2022-06-08",
+          end_date: "2022-07-07"
+      }
+  ]
   };
   // دو تا آرایه هامون رو که یکی برای سفر ها بود و یکی هم برای پرداختی ها ،با عملگر اسپریت هر دو رو باهم ادغام کردیم و آزایه ترکیب داده ها (کمبایند داتا )بر اساس ویژگی تایم (دیت تایم)به ترتیب نزولی مرتب کردیم
 
   //در دستور بازگشت داده های مرتب شده با استفاده از متد (مپ)حلقه ای درست کردیم و جزییات هر ایتم روی صفحه نمایش دادیم ،خروجی شامل داده های پرداختی وسفر ها که براساس تاریخ از جدید به قدیم مرتب شده اند
 
-  const combinedData = [...data.trip_financials, ...data.payments];
+  const combinedData = [...data.trip_financials, ...data.payments, ...data.misc_expenses,...data.concurrency_costs];
 
   //متغیر گروپ دیتا رو اضافه کردم کخ حاصلش گروه بندیه داده های ترکیبی براساس تاریخه و از روس ریدیوس استفاده کردم
 
@@ -61,16 +79,20 @@ const ExampleComponent = () => {
 
   //متغیر سورت داتا که نوشتم کلید های مرتب شده ی  گروپ دیتا  رو نگه میداره و از جدید به غدیم مرتب میکنه
   const sortedDates = Object.keys(groupedData).sort(
-    (a, b) => new Date(b).getTime() - new Date(a).getTime(),
+    (a, b,c,d) =>new Date(d).getTime()- new Date(c).getTime() - new Date(b).getTime() - new Date(a).getTime(),
   );
   //
   const [filterType, setFilterType] = useState("");
   // فانکشن فیلتر داتاو فیلتر کردن داده ها  بر اساس فیلتر تایپ
   const filteredData = (filterType) => {
     if (filterType === "payment") {
-      return combinedData.filter((item) => item.trip_id === undefined);
+      return combinedData.filter((item) => item.trip_id=== undefined);
     } else if (filterType === "trip") {
       return combinedData.filter((item) => item.trip_id !== undefined);
+     } else if (filterType === "misc_expenses") {
+        return combinedData.filter((item) => item.misc_id !== undefined);
+      } else if (filterType === "concurrency_costs") {
+        return combinedData.filter((item) => item.concurrency_id !== undefined);
     } else {
       return combinedData;
     }
@@ -84,6 +106,8 @@ const ExampleComponent = () => {
         </button>
         <button onClick={() => setFilterType("")}>Show All</button>
         <button onClick={() => setFilterType("trip")}>Filter by Trip</button>
+        <button onClick={() => setFilterType("misc_expenses")}>Filter by misc_expenses </button>
+        <button onClick={() => setFilterType("concurrency_costs")}>Filter by concurrency_costs </button>
       </div>
       {sortedDates.map((date) => (
         <div key={date}>
